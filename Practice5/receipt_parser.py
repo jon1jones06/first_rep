1e4597c842feb47d0caf3016d4443e65f3d4e517
jon1jones06 <kirilfedor34@gmail.com>
2026-03-07 14:28:32 +0500

    Add Practice5 - Python RegEx and receipt parsing examples

---
+++ Practice5/receipt_parser.py
@@ -0,0 +1,48 @@
+import re
+import os
+
+current_folder = os.path.dirname(__file__) 
+file_path = os.path.join(current_folder, 'raw.txt')
+
+def solve_tasks():
+        with open(file_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+            lines = content.splitlines()
+
+        # 1
+        print("1. 'ab*':", re.findall(r'ab*', content)[:5])
+
+        # 2
+        print("2. 'ab{2,3}':", re.findall(r'ab{2,3}', content)[:5])
+
+        # 3
+        print("3. snake_case:", re.findall(r'[a-z]+_[a-z]+', content)[:5])
+
+        # 4
+        print("4. Upper + lower:", re.findall(r'[A-Z][a-z]+', content)[:5])
+
+        # 5
+        task5 = [l for l in lines if re.search(r'^a.*b$', l)]
+        print("5. Start 'a', end 'b':", task5[:3])
+
+        # 6
+        print("6. Replace:", re.sub(r'[ ,.]', ':', content[:50]))
+
+        # 7
+        def to_camel(s):
+            words = s.split('_')
+            return words[0] + ''.join(w.capitalize() for w in words[1:])
+        
+        # 8
+        print("8. Split at Upper:", re.findall(r'[A-Z][^A-Z]*', "SplitThisString"))
+
+        # 9
+        print("9. Insert spaces:", re.sub(r"(\w)([A-Z])", r"\1 \2", "InsertSpacesHere"))
+
+        # 10
+        def to_snake(s):
+            return re.sub(r'(?<!^)(?=[A-Z])', '_', s).lower()
+        print("10. Camel to Snake:", to_snake("CamelCaseString"))
+
+if __name__ == "__main__":
+    solve_tasks()
